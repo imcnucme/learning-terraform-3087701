5543ec5b39f838ce74c56b2238b68a0a433511b9
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-08-31 12:01:14 -0400

    create new dev vpc 6

--- main.tf
+++ main.tf
@@ -36,7 +36,8 @@ module "My_Blog_vpc" {
 resource "aws_instance" "blog" {
   ami                   = data.aws_ami.app_ami.id
   instance_type         = var.instance_type
- vpc_security_group_ids = [module.blog_sg.vpc_security_group_ids[0]]
+ vpc_security_group_ids = module.blog_sg.security_group_ids
+
 
  subnet_id = module.My_Blog_vpc.public_subnets[0]
 
@@ -50,7 +51,7 @@ module "blog_sg" {
   version = "5.3.0"
   name    = "My_Blog"
 
-  vpc_id                = module.blog_vpc.vpc.id
+    vpc_id              = module.My_Blog_vpc.vpc_id
     ingress_rules       = ["http-80-tcp","https-443-tcp"]
     ingress_cidr_blocks = ["0.0.0.0/0"]
 
