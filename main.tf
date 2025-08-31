693e8a9166a0d30b2da4ef57b15bcdebb2d7ae43
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-08-31 14:37:37 -0400

    load balancer 3

--- main.tf
+++ main.tf
@@ -71,8 +71,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
+  vpc_id  = module.My_Blog_vpc.vpc_id
+  subnets = module.My_Blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
   # Security Group
