fd9e4ff55823ec3677ff545c5a7a4d093e3a6ab5
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-08-31 13:07:29 -0400

    create new dev vpc 19

--- main.tf
+++ main.tf
@@ -51,9 +51,10 @@ module "blog_sg" {
 }
 
 resource "aws_instance" "blog" {
-  ami                   = data.aws_ami.app_ami.id
-  instance_type         = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.this_security_group_id]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
 
 
  subnet_id = module.My_Blog_vpc.public_subnets[0]
