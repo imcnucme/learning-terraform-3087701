53dfd9c6616f49303119a6c3ae64ea242659c9b7
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-08-31 16:28:22 -0400

    08312025

--- main.tf
+++ main.tf
@@ -1,24 +1,69 @@
 data "aws_ami" "app_ami" {
-  most_recent = true
+most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
+filter {
+name   = "name"
+values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+filter {
+name   = "virtualization-type"
+values = ["hvm"]
 }
+
+owners = ["979382823631"] # Bitnami
+}
+
+data "aws_vpc" "default" {
+default = true
+}
+
+module "My_Blog_vpc" {
+source = "terraform-aws-modules/vpc/aws"
+
+name = "dev"
+cidr = "10.0.0.0/16"
+
+azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+tags = {
+Terraform = "true"
+Environment = "dev"
+}
+}
+
+module "blog_sg" {
+source  = "terraform-aws-modules/security-group/aws"
+version = "5.3.0"
+name    = "My_Blog"
+description = "Security group for blog instance"
+
+
+vpc_id              = module.My_Blog_vpc.vpc_id
+ingress_rules       = ["http-80-tcp","https-443-tcp"]
+ingress_cidr_blocks = ["0.0.0.0/0"]
+
+
+egress_rules       = ["all-all"]
+egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_instance" "blog" {
+  ami                   = data.aws_ami.app_ami.id
+  instance_type         = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.this_security_group_id]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+
+
+subnet_id = module.My_Blog_vpc.public_subnets[0]
+
+tags = {
+Name = "HelloWorld"
+}
+}
+
