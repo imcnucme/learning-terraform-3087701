b93c5149587cd5fe9d2a3bb575ac1f612e1fbdd2
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-08-31 18:23:09 -0400

    so2ndtime

--- main.tf
+++ main.tf
@@ -18,6 +18,21 @@ data "aws_vpc" "default" {
 default = true
 }
 
+module "My_Blog_vpc" {
+source = "terraform-aws-modules/vpc/aws"
+
+name = "dev"
+cidr = "10.0.0.0/16"
+
+azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+tags = {
+Terraform = "true"
+Environment = "dev"
+}
+}
 
 module "blog_sg" {
 source  = "terraform-aws-modules/security-group/aws"
@@ -27,7 +42,6 @@ description = "Security group for blog instance"
 
 
 vpc_id              = module.My_Blog_vpc.vpc_id
-
 ingress_rules       = ["http-80-tcp","https-443-tcp"]
 ingress_cidr_blocks = ["0.0.0.0/0"]
 
@@ -48,12 +62,3 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 }
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
->>>>>>> 3209c7f3aeaf161875ae2f7a43c9c1efe5c589
