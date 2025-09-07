0a3fe51a365d59913a47a2602ce004e4431dc2f2
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-09-07 18:12:17 -0400

    create new dev vpc 3

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_instance" "blog" {
 
   vpc_id = module.blog_vpc.vpc_id
 
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules        = ["all-all"]
