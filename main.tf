3cc90a87695801dd9103dcf583370734a060bc98
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-09-07 18:19:03 -0400

    create new dev vpc 4

--- main.tf
+++ main.tf
@@ -48,17 +48,14 @@ resource "aws_instance" "blog" {
   }
 }
 
-
-
   module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
 
-  name = "blog"
-
   vpc_id = module.blog_vpc.vpc_id
+  name   = "blog"
 
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_rules       = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules        = ["all-all"]
