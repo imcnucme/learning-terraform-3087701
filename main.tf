0ad6a5edd3d633bcca5d3423899c87ff4a552fa9
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-09-08 23:47:24 -0400

    lb 13.5

--- main.tf
+++ main.tf
@@ -14,6 +14,18 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.micro"
+  subnet_id     = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  tags = {
+    Name = "blog-instance"
+    Environment = "dev"
+  }
+}
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
