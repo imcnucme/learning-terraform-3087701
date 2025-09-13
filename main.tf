e97acd5cc1450b8bfc576e1813dfffa9ddbe7cfb
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-09-13 16:57:14 -0400

    associate public ip address

--- main.tf
+++ main.tf
@@ -20,6 +20,9 @@ resource "aws_instance" "blog" {
   subnet_id     = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  associate_public_ip_address = true
+
+
   tags = {
     Name = "Learning Terraform"
     Environment = "dev"
