e2afd9d56b28e38d551ef11e8656c01f9dfc2a47
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-09-13 17:51:34 -0400

    autoscaling

--- main.tf
+++ main.tf
@@ -14,20 +14,22 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.1"
+  
+  name      = "blog"
+  min_size  = 1
+  max_size  = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id
+
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  subnet_id     = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  associate_public_ip_address = true
-
-
-  tags = {
-    Name = "Learning Terraform"
-    Environment = "dev"
-  }
 }
+
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
