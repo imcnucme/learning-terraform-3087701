a5114e9fb587913ad1e98f4d82858d70156941a0
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-09-13 16:37:53 -0400

    create new dev vpc

--- main.tf
+++ main.tf
@@ -16,9 +16,11 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
   subnet_id     = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "blog-instance"
@@ -41,6 +43,17 @@ module "blog_vpc" {
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
@@ -76,14 +89,3 @@ module "blog_alb" {
   }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
-
-  vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-}
