f3edd13780c6665533d299be4a488097139c8055
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-09-13 18:17:28 -0400

    autoscaling 2.5

--- main.tf
+++ main.tf
@@ -27,10 +27,6 @@ module "autoscaling" {
 
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  target_group_arns   = module.blog_alb.target_group_arns
-
-  
 }
 
 module "blog_alb" {
