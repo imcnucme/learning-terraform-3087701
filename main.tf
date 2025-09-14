9e110d42e5907ea640f1e005cdfc52631ca8c78b
imcnucme <79542451+imcnucme@users.noreply.github.com>
2025-09-13 21:40:26 -0400

    associate_ip_n_asg_3.5

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.1"
+  version = "6.5.2"
   
   name      = "blog"
   min_size  = 1
